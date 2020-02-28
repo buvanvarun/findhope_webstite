0ea4def798048e9d5b3dc7dba93eae73d612220f
gnanasurya <gnanasurya2@gmail.com>
2020-02-28 22:52:37 +0530

    keywords

---
+++ college.js
@@ -0,0 +1,55 @@
+function onclik() {
+    if (document.getElementById("menu").style.visibility == "hidden") {
+        document.getElementById("menu1").style.top = "-120vw";
+        setTimeout(function () {
+            document.getElementById("menu2").style.top = "-120vw";
+        }, 200);
+        setTimeout(function () {
+            document.getElementById("menu").style.visibility = "visible";
+        }, 200);
+    } else {
+        document.getElementById("menu1").style.top = "-250vw";
+        setTimeout(function () {
+            document.getElementById("menu2").style.top = "-250vw";
+        }, 200);
+        setTimeout(function () {
+            document.getElementById("menu").style.visibility = "hidden";
+        }, 200);
+    }
+}
+
+var i = 0;
+var txt = "National Institute of Technology Agartala";
+var speed = 100;
+
+function addSymbols() {
+    document.getElementById("title").innerHTML = document
+        .getElementById("title")
+        .innerHTML.substr(
+            0,
+            document.getElementById("title").innerHTML.length - 38
+        );
+}
+
+function typeWriter() {
+    var symbols = ["*", "-", "$", "%", "?", "@"];
+    if (i < txt.length) {
+        var sym = symbols[Math.floor(Math.random() * 5)];
+        if (txt.charAt(i) == "Z" || txt.charAt(i) == "Q") {
+            document.getElementById("title").innerHTML +=
+                '<span style="color: #95FFD3;">' + txt.charAt(i) + "</span>" + sym;
+        } else {
+            document.getElementById("title").innerHTML += txt.charAt(i) + sym;
+        }
+
+        document.getElementById("title").innerHTML = document
+            .getElementById("title")
+            .innerHTML.replace(
+                sym,
+                '<span style="color: #95FFD3;">' + sym + "</span>"
+            );
+        i++;
+        setTimeout(addSymbols, speed);
+        setTimeout(typeWriter, speed);
+    }
+}
