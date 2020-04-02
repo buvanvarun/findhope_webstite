435a2b91451ca51d14880699420e84394adb06d1
gnanasurya <gnanasurya2@gmail.com>
2020-04-02 23:17:02 +0530

    made changes in resourecs page

--- questions.js
+++ questions.js
@@ -1,54 +1,13 @@
-var currentTab = 0;
-showTab(currentTab);
-
-function showTab(n) {
-    var x = document.getElementsByClassName('test_container');
-    document.getElementById('warn').innerHTML = ''
-    x[n].style.display = 'flex';
-    if (n == (x.length - 1)) {
-        document.getElementById('button').innerHTML = 'Submit'
-        resultHandler();
+function expand(num) {
+    var idName = "hide" + num;
+    var iconName = "icon" + num;
+    if (document.getElementById(idName).style.display === '' || document.getElementById(idName).style.display === 'none') {
+        console.log("AS");
+        document.getElementById(idName).style.display = 'block';
+        document.getElementById(iconName).classList.add('plusI');
+    } else {
+        console.log("As");
+        document.getElementById(idName).style.display = 'none';
+        document.getElementById(iconName).classList.remove('plusI');
     }
-}
-
-function nextQuestion() {
-    var x = document.getElementsByClassName("test_container");
-    if (!validateForm()) {
-        return false;
-    }
-    x[currentTab].style.display = 'none';
-    currentTab += 1;
-    if (currentTab >= x.length) {
-        return false;
-    }
-    console.log(currentTab)
-    showTab(currentTab);
-}
-
-function validateForm() {
-    var x, y, i, valid = true;
-    x = document.getElementsByClassName('test_container')
-    y = x[currentTab].getElementsByTagName('textarea');
-    for (i = 0; i < y.length; i++) {
-        if (y[i].value == '') {
-            document.getElementById('warn').innerHTML = 'Answer all the questions to continue';
-            valid = false
-        }
-    }
-    return valid
-}
-
-function resultHandler() {
-    console.log("asfkas");
-    var result = document.getElementById('result');
-    var x = document.getElementsByClassName('test_container');
-    for (var i = 0; i < x.length; i++) {
-        var y = x[i].getElementsByTagName('textarea');
-        for (var j = 0; j < y.length; j++) {
-            var p = document.createElement('p');
-            p.innerHTML = y[j].value;
-            result.appendChild(p);
-        }
-    }
-    result.style.display = 'block';
 }
