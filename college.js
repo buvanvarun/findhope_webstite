4844fb23b92f46690bd1532352d0fdabdc797215
gnanasurya <gnanasurya2@gmail.com>
2020-04-09 23:17:20 +0530

    made changes in college and ikigai page

--- college.js
+++ college.js
@@ -1,18 +1,18 @@
 function onclik() {
   if (document.getElementById("menu").style.visibility == "hidden") {
     document.getElementById("menu1").style.top = "-120vw";
-    setTimeout(function() {
+    setTimeout(function () {
       document.getElementById("menu2").style.top = "-120vw";
     }, 200);
-    setTimeout(function() {
+    setTimeout(function () {
       document.getElementById("menu").style.visibility = "visible";
     }, 200);
   } else {
     document.getElementById("menu1").style.top = "-250vw";
-    setTimeout(function() {
+    setTimeout(function () {
       document.getElementById("menu2").style.top = "-250vw";
     }, 200);
-    setTimeout(function() {
+    setTimeout(function () {
       document.getElementById("menu").style.visibility = "hidden";
     }, 200);
   }
@@ -70,29 +70,23 @@ function pressed(number) {
 }
 
 title = [
-  "OC Team Recruitment",
-  "Talk on Social Entrepreunership",
-  "Q & A Session"
+  "Awareness Champaign.",
+  "Literacy Campaign",
+  "Stigma busting Campaign",
 ];
-time = ["10:00am", "2:00pm", "5:00pm"];
-date = [
-  "Saturday, November 2, 2019",
-  "Sunday, November 3, 2019",
-  "Thursday, November 7, 2019"
+date = ["April 1st week", " April 2nd week", "April 3rd week"];
+venue = [
+  "Hyper local model",
+  "Grassroot Education model",
+  "contact based education model",
 ];
-venue = ["E-CELL Class Room", "M100 RIC Building", "Auditorium"];
-additional_info = [
-  "Brainstorming, Team Structure, Recruitment",
-  "Speaker-SHARAD SAGAR (Global) forbes 30 under 30 social entrepreneur.",
-  "Guest- SWARAJ PRIYADARSHI (DIANA Prize Winner) (Social Entrepreunership)"
-];
+additional_info = ["400", "100", "150"];
 
 function content(number, r1, r2) {
   document.getElementById("button" + (number + 1)).style.opacity = "1";
   document.getElementById("button" + (r1 + 1)).style.opacity = "0.2";
   document.getElementById("button" + (r2 + 1)).style.opacity = "0.2";
   document.getElementById("content_title").innerHTML = title[number];
-  document.getElementById("time").innerHTML = time[number];
   document.getElementById("date").innerHTML = date[number];
   document.getElementById("venue").innerHTML = venue[number];
   document.getElementById("info").innerHTML = additional_info[number];
