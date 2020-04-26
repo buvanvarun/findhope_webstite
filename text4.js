40979acea41404c24254cfdbaa421da1380ae250
gnanasurya <gnanasurya2@gmail.com>
2020-04-26 19:44:52 +0530

    added new tests

--- text4.js
+++ text4.js
@@ -1,12 +1,15 @@
 (function () {
   var questions = [
-    'Feeling <span style="color: #70db70;">nervous</span>, anxious, or on edge',
-    'Not being able to stop or control <span style="color: #70db70;">worrying</span>',
-    '<span style="color: #70db70;">Worrying</span> too much about different things',
-    'Trouble <span style="color: #70db70;">relaxing</span>',
-    'Being so <span style="color: #70db70;">restless</span> that it\'s hard to sit still',
-    'Becoming easily <span style="color: #70db70;">annoyed</span> or irritable',
-    'Feeling <span style="color: #70db70;">afraid</span> as if something awful might happen ',
+    'In the last month, how often have you beens <span style="color:#70DB70">upset</span> because of something that  happened unexpectedly?',
+    'In the last month, how often have you felt that you were unable to <span style="color:#70DB70">control</span> the important things in your life?',
+    ' In the last month, how often have you felt <span style="color:#70DB70">nervous</span> and <span style="color:#70DB70">stressed</span>?',
+    'In the last month, how often have you felt <span style="color:#70DB70">confident</span> about your ability to handle  your personal problems?',
+    ' In the last month, how often have you felt that things were going <span style="color:#70DB70">your way?</span>',
+    'In the last month, how often have you found that you could not <span style="color:#70DB70">cope</span> withall the things that you had to do?',
+    'In the last month, how often have you been able to <span style="color:#70DB70">control irritations</span> inyour life?',
+    'In the last month, how often have you felt that you were on <span style="color:#70DB70">top of things</span>?',
+    'In the last month, how often have you been <span style="color:#70DB70">angered</span> because of things that happened that were outside of your control?',
+    'In the last month, how often have you felt <span style="color:#70DB70">difficulties</span> were piling up so high thatyou could not overcome them?',
   ];
 
   var ch = 0;
@@ -36,24 +39,39 @@
   var o2 = document.getElementById("o2");
   var o3 = document.getElementById("o3");
   var o4 = document.getElementById("o4");
-
+  var o5 = document.getElementById("o5");
+  const reversAnswer = (i) => {
+    depressionCount += 4 - i;
+  };
   o1.onclick = function () {
     reasons[ch] = 0;
+    if (ch === 3 && ch === 4 && ch === 6 && ch === 7) {
+      reversAnswer(0);
+    }
     displayQuestions();
   };
   o2.onclick = function () {
     reasons[ch] = 1;
     depressionCount += 1;
+    if (ch === 3 && ch === 4 && ch === 6 && ch === 7) {
+      reversAnswer(0);
+    }
     displayQuestions();
   };
   o3.onclick = function () {
     reasons[ch] = 2;
     depressionCount += 2;
+    if (ch === 3 && ch === 4 && ch === 6 && ch === 7) {
+      reversAnswer(0);
+    }
     displayQuestions();
   };
   o4.onclick = function () {
     reasons[ch] = 3;
     depressionCount += 3;
+    if (ch === 3 && ch === 4 && ch === 6 && ch === 7) {
+      reversAnswer(0);
+    }
     displayQuestions();
   };
 })();
@@ -63,22 +81,20 @@ function displayResult(s, reasons) {
   document.getElementById("options").classList.add("opac");
   document.getElementById("res").classList.remove("opac");
   var str;
-  if (s <= 5) {
-    str = "Minimal anxiety";
-  } else if (s > 5 && s <= 9) {
-    str = "Mild anxiety";
-  } else if (s > 9 && s <= 14) {
-    str = "moderate anxiety";
+  if (s <= 13) {
+    str = "Low Stress";
+  } else if (s > 13 && s <= 26) {
+    str = "Moderate Stress";
   } else {
-    str = "severe anxiety";
+    str = "High perceived Stress";
   }
 
   document.getElementById("r").innerHTML = "Result:\n" + str;
   document.getElementById("percent").innerHTML =
-    Math.ceil((s / 21) * 100).toString() + "%";
+    Math.ceil((s / 30) * 100).toString() + "%";
   document.getElementById("circle2").style.strokeDashoffset = (
     660 *
-    (s / 21)
+    (s / 30)
   ).toString();
   console.log(s);
   var items = Object.keys(reasons).map(function (key) {
